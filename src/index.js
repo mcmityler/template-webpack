1323d5f47faf88550d20981dc85cd56ade29f2d2
Tyler McMillan <36271467+mcmityler@users.noreply.github.com>
2026-06-28 14:11:13 -0400

    Add template files

---
+++ src/index.js
@@ -0,0 +1,13 @@
+// src/index.js
+import "./styles.css";
+import { greeting } from "./greeting.js";
+
+console.log(greeting);
+
+// src/index.js
+import testImage from "./ramenTest.jpg";
+
+const image = document.createElement("img");
+image.src = testImage;
+
+document.body.appendChild(image);
